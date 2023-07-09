92b2a640b1d93ab6391c4666841f07c688e56ca3
hasan <hasanisamiel19951995@gmail.com>
2023-07-09 08:00:38 +0300

    answers for my test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,24 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $data = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email')
+        ];
+        
+        if($request->has('password')){
+            auth()->user()->update->array_merge([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => $request->password
+            ]);
+        }else{
+            auth()->user()->update->array_merge([
+                'name' => $request->input('name'),
+                'email' => $request->input('email')
+            ]);
+        }
+              
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
